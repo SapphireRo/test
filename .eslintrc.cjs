0824aa599c0c37029df1b02f565096def8121a90
张震霆 <2426446427@qq.com>
2023-05-22 21:08:25 +0800

    [ bug ]测试下

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,12 +3,68 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+  env: {
+    browser: true,
+    node: true,
+    es2021: true
+  },
+  //it is base on https://github.com/vuejs/eslint-config-vue
+  rules: {
+    camelCase: [
+      0,
+      {
+        properties: 'always'
+      }
+    ], //强制驼峰法命名 - 关闭
+    'eol-last': 0, //文件以单一的换行符结束 - 打开
+    eqeqeq: [
+      'error',
+      'always',
+      {
+        null: 'ignore'
+      }
+    ], //必须使用全等  0关 1警告 2或者error错误
+    'no-const-assign': 2, //禁止修改const声明的变量 - 开启
+    'no-empty': 0, //块语句中的内容不能为空 - 关闭
+    'no-spaced-func': 2, //函数调用时 函数名与()之间不能有空格 - 开启
+    'no-undef': 2, //不能有未定义的变量 - 打开
+    'no-unused-vars': [
+      2,
+      {
+        vars: 'all',
+        args: 'none'
+      }
+    ], //不能有声明后未被使用的变量或参数 0关 1警告 2声明不使用就爆红
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'never',
+          normal: 'any',
+          component: 'any'
+        },
+        svg: 'always',
+        math: 'always'
+      }
+    ], //html使用单闭合标签 -打开（配置：在html中never从不开启）
+    'space-before-function-paren': [0, 'never'], //函数定义时括号前面要不要有空格：设置为0就是关闭这个规则的检验 1就是如不符合规则就警告 2就是如不符合就报错 --至于保存代码时候 是否会生成空格 这个取决于你设置的代码风格（例如prettier）
+    'prefer-const': 0, //首选const -- 关闭（就可以用var let const）
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'object-curly-spacing': [
+      2,
+      'always',
+      {
+        objectsInObjects: false
+      }
+    ],
+    'array-bracket-spacing': [2, 'never']
   }
 }
--- src/entry/EntryLogin.vue
+++ src/entry/EntryLogin.vue
@@ -1,23 +1,61 @@
 <template>
-  <el-skeleton style="width: 240px">
-    <template #template>
-      <el-skeleton-item variant="image" style="width: 240px; height: 240px" />
-      <div style="padding: 14px">
-        <el-skeleton-item variant="p" style="width: 50%" />
-        <div style="
-            display: flex;
-            align-items: center;
-            justify-items: space-between;
-          ">
-          <el-skeleton-item variant="text" style="margin-right: 16px" />
-          <el-skeleton-item variant="text" style="width: 30%" />
-        </div>
-      </div>
-    </template>
-  </el-skeleton>
+  <main class="main-container">
+    <article class="lyric">
+      <ul v-for="(item, index) in LyricsArr" :key="index">
+        <li :class="['act', { active: isActive }]">{{ item.lrc }}</li>
+      </ul>
+    </article>
+    <audio ref="audioLrc" :src="lyricLrc" controls class="audio" currentTime />
+    <button type="button" @click="getLyricsArr()">1</button>
+  </main>
 </template>
 
-<script lang="jsx" setup></script>
+<script lang="jsx" setup>
+import { ref, watchEffect } from 'vue'
+const isActive = ref(true)
+const LyricsArr = ref([])
+const audioLrc = ref()
+const lyricLrc = ref('/src/music/.bomdo.mp3')
+// eslint-disable-next-line no-unused-vars
+const changeButton = () => {
+  isActive.value = !isActive.value
+}
+
+// eslint-disable-next-line no-unused-vars
+function getLyricsArr() {
+  const lyrics = `[00:00.000] My Heart Will Go On
+  [00:12.345] Celine Dion
+  [00:23.456] Titanic Soundtrack
+  [00:34.567] Andy
+  [00:45.678]
+  [01:23.456] Every night in my dreams
+  [01:29.876] I see you, I feel you
+  [01:36.789] That is how I know you go on`
+  lyrics.split('\n').forEach((line) => {
+    const [time, Str] = line.match(/^\[(.*?)\]\s*(.*)/).slice(1) ?? "ull";
+    const [minutes, seconds] = time.split(':');
+    const timeCode = +minutes * 60 + +seconds;
+    var parseLyrics = {
+      time: timeCode,
+      lrc: Str
+    };
+    LyricsArr.value.push(parseLyrics);
+    console.log(parseLyrics);
+  })
+}
+// eslint-disable-next-line no-unused-vars
+function findIndex() {
+  for (var i = 0; i < LyricsArr.value.length; i++) {
+    if (audioLrc.value.currentTime < LyricsArr[i].time) {
+      return i - 1;
+    }
+    return LyricsArr.value.length - 1;
+  }
+}
+
+watchEffect(() => {
+})
+</script>
 <style lang="less" scoped>
 @import url(LoginVue.less);
 </style>
